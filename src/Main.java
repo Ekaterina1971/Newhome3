94dc09f12d659dec2a3fd0ee47ea911607e24350
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-09 20:39:51 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int clientOs = 1;
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения iOS по ссылке.");
+        }
+        if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
     }
 }
